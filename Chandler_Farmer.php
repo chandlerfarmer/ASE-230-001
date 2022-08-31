1018edaa74d0a545a97665ca3b2f1b6d37f0439a
chandlerfarmer <63524553+chandlerfarmer@users.noreply.github.com>
2022-08-31 18:03:47 -0400

    Add files via upload

---
+++ Chandler_Farmer.php
@@ -0,0 +1,93 @@
+<html lang="en">
+  <!-- https://www.bootdey.com/snippets/view/team-user-resume#html -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" integrity="sha256-mmgLkCYLUQbXn0B1SRqzHar6dCnv9oZFPEC1g1cwlkk=" crossorigin="anonymous" />
+	
+  <body>
+    <link rel="stylesheet" href="detail.css" />
+    <title> <?php echo "ASE 230 - Chandler Farmer" ?></title>
+    <div class="container text-center mb-5">
+      <h1> <?php echo "This is ASE 230 - Chandler Farmer" ?></h1>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-5 col-md-6">
+          <div class="mb-2">
+          <img class="w-100" <?php echo 'src="grin smile.png"'?>>
+          </div>
+          <div class="mb-2 d-flex">
+            <h4 class="font-weight-normal"> <?php echo "Chandler Farmer" ?></h4>
+            <div class="social d-flex ml-auto">
+              <p class="pr-2 font-weight-normal"><?php echo 'Follow on:'?></p>
+              <a <?php echo 'href="https://www.instagram.com/chandler.farmer/" class="text-muted mr-1"'?>>
+                <i class="fab fa-instagram"></i>
+              </a>
+              <a <?php echo 'href="https://www.linkedin.com/in/chandlerfarmer" class="text-muted"'?>>
+                <i class="fab fa-linkedin"></i>
+              </a>
+            </div>
+          </div>
+          <div class="mb-2">
+            <ul class="list-unstyled">
+              <li class="media">
+                <span class="w-25 text-black font-weight-normal"> <?php echo "Dream Job:" ?></span>
+                <label class="media-body"> <?php echo "Cybersecurity Risk & Compliance Manager" ?></label>
+              </li>
+              <li class="media">
+                <span class="w-25 text-black font-weight-normal"> <?php echo "Dream Company:"?></span>
+                <label class="media-body"> <?php echo "Microsoft" ?></label>
+              </li>
+              <li class="media">
+                <span class="w-25 text-black font-weight-normal"> <?php echo "Email:"?></span>
+                <label class="media-body"> <?php echo "farmerc4@mymail.nku.edu"?></label>
+              </li>
+            </ul>
+          </div>
+        </div>
+      <div class="col-lg-7 col-md-6 pl-xl-3">
+        <h5 class="font-weight-normal"><?php echo "Introducing Me"?></h5>
+        <p><?php echo "My name is Chandler Farmer and I'm a senior at Northern Kentucky University studying Cybersecurity. I'm an Information Technology Risk & Compliance Intern at American Modern Insurance Group. During my free time I enjoy working out, spending time with friends, and watching movies."?></p>
+          <div class="my-2 bg-light p-2">
+            <p class="font-italic mb-0"> <?php echo '"Learning never exhausts the mind" - Leonardo da Vinci'?></p>
+          </div>
+          <div class="mb-2 mt-2 pt-1">
+            <h5 class="font-weight-normal"> <?php echo "Top skills"?></h5>
+          </div>
+          <div class="py-1">
+            <div class="progress">
+              <div class="progress-bar" role="progressbar" style="width:85%" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar-title"><?php echo "Computer Security"?></div>
+              <span class="progress-bar-number"></span>
+              </div>
+            </div>
+          </div>
+          <div class="py-1">
+            <div class="progress">
+              <div class="progress-bar" role="progressbar" style="width:70%" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar-title"><?php echo "Computer Programming"?></div>
+                <span class="progress-bar-number"></span>
+              </div>
+            </div>
+          </div>
+          <div class="py-1">
+            <div class="progress">
+              <div class="progress-bar" role="progressbar" style="width:77%" aria-valuenow="77" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar-title"><?php echo "Risk Management"?></div>
+                <span class="progress-bar-number"></span>
+              </div>
+            </div>
+          </div>
+          <h5 class="font-weight-normal"><?php echo "Fun fact"?></h5>
+          <p><?php echo "This summer I traveled to Gatlinburg Tennessee & Norris Lake (Tennessee). Over winter break I'm traveling to Florida. Lastly, during spring break I'm taking a cruise to the Bahamas."?></p>
+          <ul class="nav nav-tabs">
+            <li class="nav-item">
+              <a class="nav-link active"href="index.php"> <?php echo "Home Page"?></a>
+            </li>
+         </ul>
+        </div>
+      </div>
+    </div>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  
